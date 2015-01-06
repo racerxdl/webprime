af169acfb03a6e01a550aa4998d035cfef504bda
Lucas Teske <lucas@teske.com.br>
2015-01-06 03:10:59 -0200

    Added Loader and Prototype to main.js

---
+++ Game/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+    <title>WebPrime</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <script>
+    var params = {
+        "scripts"   : ["../Libraries/main.js"],
+        "css"       : ["../CSS/webprime.css"]
+    };
+    </script>
+</head>
+<body>
+    <a href="https://github.com/racerxdl/webprime"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></a>
+    <div id="content"></div>
+    <script src="../Libraries/loader.js"></script>
+</body>
+</html>
